c24057edf23e53aec5663fa5b28922bf819e3850
jenbroatch <54128714+jenbroatch@users.noreply.github.com>
2023-01-31 10:52:40 -0700

    Add files via upload

--- Module2Script.R
+++ Module2Script.R
@@ -1,19 +1,69 @@
 library(dplyr)
 
 anorexia<- read.csv("https://raw.githubusercontent.com/jenbroatch/STP281/master/Anorexia.csv")
-
+View(anorexia)
+par(mfrow = c(1, 1)) # do not combine plots
 boxplot(anorexia$Cognitive.Therapy, anorexia$Family.Therapy, anorexia$Control,
-        names=c("Cognitive Therapy", "Family Therapy", "Control"), 
+        names=c("Cogn. Therapy", "Family Therapy", "Control"), 
         ylab="Weight Loss/Gain (lbs)") 
 
-anorexia_stack=na.omit(stack(anorexia) 
+anorexia_stack=na.omit(stack(anorexia))
 View(anorexia_stack)
 
 
 anovamod =aov(values~ind, data=anorexia_stack)
 summary(anovamod) 
 
-#lab
+#Quick check of normality assumption
+par(mfrow = c(1, 2)) # combine plots
+
+# histogram
+hist(anovamod$residuals)
+
+# QQ-plot
+library(car)
+qqPlot(anovamod$residuals,
+       id = FALSE # id = FALSE to remove point identification
+)
+
+#Another choice
+par(mfrow = c(1, 2)) # combine plots
+
+# 1. Homogeneity of variances
+plot(anovamod, which = 3)
+
+# 2. Normality
+plot(anovamod, which = 2)
+
+#Test for normality
+shapiro.test(anovamod$residuals) 
+#Levene's Test
+
+library(car)
+
+leveneTest(values~ind, data=anorexia_stack)
+
+
+
+
+
+
+#Module 2.2 Multiple comparisons
+
+TukeyHSD(anovamod, conf.level=.95) 
+plot(TukeyHSD(anovamod, conf.level=.95), las = 2)
+
+library(multcomp)
+
+# Tukey HSD test:
+post_test <- glht(anovamod,
+                  linfct = mcp(ind = "Tukey")
+)
+summary(post_test)
+par(mfrow = c(1, 1)) # combine plots
+plot(post_test)
+
+#Module 1: Problem Set dcript 
 # Get the Data
 
 # Read in with tidytuesdayR package 
@@ -38,4 +88,3 @@ t.test(youtube2$funny, youtube2$patriotic)
 
 
 
-mutate(gradebook, Pass.Fail = ifelse(grade > 60, "Pass", "Fail"))

51736ca8234261c0b43e6bcbcc1030ace57d7571
jenbroatch <54128714+jenbroatch@users.noreply.github.com>
2023-04-06 10:08:44 -0700

    Add files via upload

--- Module5Script.R
+++ Module5Script.R
@@ -22,3 +22,24 @@ res1
 res1$expected 
 res1$stdres
 
+#Chi squared test of independence 
+Observed <- cbind(c(2, 39, 131, 175, 78), c(8, 113, 138, 129, 32), 
+                  c(46, 202, 120, 65,  8))
+
+Observed
+rownames(Observed)=c("Post-Grad Degree", "College Degree", "Some College", 
+                     "HS Grad", "No HS Degree")
+
+colnames(Observed)=c("<$30k","$30k to $75k", " >$75k")
+Observed
+
+prop.table(Observed,2)
+barplot(prop.table(Observed,2),beside=TRUE,legend.text=TRUE,
+        ylim=c(0,1),ylab="Proportions")
+
+x=addmargins(Observed)
+x
+res1=chisq.test(Observed)
+res1
+res1$expected 
+res1$stdres

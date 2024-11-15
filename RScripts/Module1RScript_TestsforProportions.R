d42402d235a8de0b2392ea7e1faf71800139e204
jenbroatch <54128714+jenbroatch@users.noreply.github.com>
2024-11-15 09:31:43 -0700

    Add files via upload

---
+++ RScripts/Module1RScript_TestsforProportions.R
@@ -0,0 +1,76 @@
+#STP281 
+#Module 1 - R Script One and Two Sample Proportions 
+
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+#Module 1.1: One Sample Confidence intervals for Proportion
+binom.confint(142, n = 1432)
+binom.confint(142, n = 1432, conf.level=.95, methods="asymptotic")
+
+
+#Module 1.2- Sample Size Calculations for Proportions
+sample.size.prop(0.01, P = 0.97, N = Inf, level = 0.95)  #Proportion known 
+sample.size.prop(0.05, P = 0.5, N = Inf, level = 0.95)   #Conservative approach 
+
+
+#Module 1.3- One Sample Proportion- Hypothesis Test Basics
+#Module 1.4- One Sample Proportion- P-value
+
+
+Flintdata<- read.csv("https://raw.githubusercontent.com/jenbroatch/STP281/refs/heads/main/DataSets/Flint.csv")
+View(Flintdata)
+counts<- table(Flintdata$Contaminated)
+counts 
+
+barplot(counts, main="Flint Water Contamination",
+        xlab="Number of Contaminated Homes") 
+barplot(counts, main="Flint Water Contamination",
+        xlab="Number of Contaminated Homes", names.arg=c("No Lead Contamination Evident", 
+                                                         "Lead Contamination Evident"), 
+        col=c("red", "blue"))
+
+
+prop.test(54, 271, p = 0.10, alternative = "greater")
+
+prop.test(25, 271, p = 0.10, alternative = "greater")
+
+#No Continuity Correction
+prop.test(54, 271, p = 0.10, alternative = "greater", correct=F)
+
+Teststat=((54/271)-.1)/sqrt(.1*.9/271)
+Teststat
+
+#Note that the square of a variable with a standard normal distribution 
+#is a Chi-squared distribution.
+Teststat^2
+
+#Module 1.5- Two Sample Proportions
+resumedata<- read.csv("https://raw.githubusercontent.com/jenbroatch/STP281/refs/heads/main/DataSets/resume.csv")
+View(resumedata)
+
+
+#Filter the dataset to include only the desired variables for example
+resume<-resumedata %>% select('race','received_callback')
+results <- data.frame(table(resume))
+results 
+
+# Vector of counts meeting the condition for each group
+counts <- c(246, 164)
+
+# Vector of sample sizes for each group
+n <- c(2445, 2445)
+
+# Find and store results
+testResult <- prop.test(x=counts, 
+                        n=n, 
+                        alternative="two.sided", 
+                        correct=FALSE)
+
+# Print z-test statistic and p-value
+unname(sqrt(testResult$statistic))
+testResult$p.value
+
+testResult 
+
